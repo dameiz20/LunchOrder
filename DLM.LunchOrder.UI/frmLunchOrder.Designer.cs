b481a39f0efd831d25dbd3f114e7c7f15f46f9f9
dameiz20 <dakotameisel25@gmail.com>
2024-02-02 23:05:47 -0600

    Added Methods and changed code to help with flow

--- DLM.LunchOrder.UI/frmLunchOrder.Designer.cs
+++ DLM.LunchOrder.UI/frmLunchOrder.Designer.cs
@@ -28,11 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            groupBox1 = new GroupBox();
+            grpmainCourse = new GroupBox();
             rbtnSalad = new RadioButton();
             rbtnPizza = new RadioButton();
             rbtnHamburger = new RadioButton();
-            grpbxAddOnItems = new GroupBox();
+            grpaddOn = new GroupBox();
             ckbxOption9 = new CheckBox();
             ckbxOption8 = new CheckBox();
             ckbxOption7 = new CheckBox();
@@ -51,83 +51,88 @@
             label1 = new Label();
             btnPlaceOrder = new Button();
             button2 = new Button();
-            groupBox1.SuspendLayout();
-            grpbxAddOnItems.SuspendLayout();
+            grpmainCourse.SuspendLayout();
+            grpaddOn.SuspendLayout();
             groupBox3.SuspendLayout();
             SuspendLayout();
             // 
-            // groupBox1
+            // grpmainCourse
             // 
-            groupBox1.Controls.Add(rbtnSalad);
-            groupBox1.Controls.Add(rbtnPizza);
-            groupBox1.Controls.Add(rbtnHamburger);
-            groupBox1.Location = new Point(51, 59);
-            groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(250, 152);
-            groupBox1.TabIndex = 0;
-            groupBox1.TabStop = false;
-            groupBox1.Text = "Main Course";
+            grpmainCourse.Controls.Add(rbtnSalad);
+            grpmainCourse.Controls.Add(rbtnPizza);
+            grpmainCourse.Controls.Add(rbtnHamburger);
+            grpmainCourse.Location = new Point(45, 44);
+            grpmainCourse.Margin = new Padding(3, 2, 3, 2);
+            grpmainCourse.Name = "grpmainCourse";
+            grpmainCourse.Padding = new Padding(3, 2, 3, 2);
+            grpmainCourse.Size = new Size(219, 114);
+            grpmainCourse.TabIndex = 0;
+            grpmainCourse.TabStop = false;
+            grpmainCourse.Text = "Main Course";
             // 
             // rbtnSalad
             // 
             rbtnSalad.AutoSize = true;
-            rbtnSalad.Location = new Point(20, 96);
+            rbtnSalad.Location = new Point(18, 72);
+            rbtnSalad.Margin = new Padding(3, 2, 3, 2);
             rbtnSalad.Name = "rbtnSalad";
-            rbtnSalad.Size = new Size(116, 24);
+            rbtnSalad.Size = new Size(91, 19);
             rbtnSalad.TabIndex = 2;
             rbtnSalad.TabStop = true;
             rbtnSalad.Text = "Salad - $4.95";
             rbtnSalad.UseVisualStyleBackColor = true;
-            rbtnSalad.CheckedChanged += rbtnSalad_CheckedChanged;
             // 
             // rbtnPizza
             // 
             rbtnPizza.AutoSize = true;
-            rbtnPizza.Location = new Point(20, 66);
+            rbtnPizza.Location = new Point(18, 50);
+            rbtnPizza.Margin = new Padding(3, 2, 3, 2);
             rbtnPizza.Name = "rbtnPizza";
-            rbtnPizza.Size = new Size(113, 24);
+            rbtnPizza.Size = new Size(89, 19);
             rbtnPizza.TabIndex = 1;
             rbtnPizza.TabStop = true;
             rbtnPizza.Text = "Pizza - $5.95";
             rbtnPizza.UseVisualStyleBackColor = true;
-            rbtnPizza.CheckedChanged += rbtnPizza_CheckedChanged;
             // 
             // rbtnHamburger
             // 
             rbtnHamburger.AutoSize = true;
-            rbtnHamburger.Location = new Point(20, 36);
+            rbtnHamburger.Location = new Point(18, 27);
+            rbtnHamburger.Margin = new Padding(3, 2, 3, 2);
             rbtnHamburger.Name = "rbtnHamburger";
-            rbtnHamburger.Size = new Size(155, 24);
+            rbtnHamburger.Size = new Size(124, 19);
             rbtnHamburger.TabIndex = 0;
             rbtnHamburger.TabStop = true;
             rbtnHamburger.Text = "Hamburger - $6.95";
             rbtnHamburger.UseVisualStyleBackColor = true;
-            rbtnHamburger.CheckedChanged += rbtnHamburger_CheckedChanged;
             // 
-            // grpbxAddOnItems
+            // grpaddOn
             // 
-            grpbxAddOnItems.Controls.Add(ckbxOption9);
-            grpbxAddOnItems.Controls.Add(ckbxOption8);
-            grpbxAddOnItems.Controls.Add(ckbxOption7);
-            grpbxAddOnItems.Controls.Add(ckbxOption6);
-            grpbxAddOnItems.Controls.Add(ckbxOption5);
-            grpbxAddOnItems.Controls.Add(ckbxOption4);
-            grpbxAddOnItems.Controls.Add(ckbxOption3);
-            grpbxAddOnItems.Controls.Add(ckbxOption2);
-            grpbxAddOnItems.Controls.Add(ckbxOption1);
-            grpbxAddOnItems.Location = new Point(412, 59);
-            grpbxAddOnItems.Name = "grpbxAddOnItems";
-            grpbxAddOnItems.Size = new Size(251, 152);
-            grpbxAddOnItems.TabIndex = 1;
-            grpbxAddOnItems.TabStop = false;
-            grpbxAddOnItems.Text = "Add-On Items";
+            grpaddOn.Controls.Add(ckbxOption9);
+            grpaddOn.Controls.Add(ckbxOption8);
+            grpaddOn.Controls.Add(ckbxOption7);
+            grpaddOn.Controls.Add(ckbxOption6);
+            grpaddOn.Controls.Add(ckbxOption5);
+            grpaddOn.Controls.Add(ckbxOption4);
+            grpaddOn.Controls.Add(ckbxOption3);
+            grpaddOn.Controls.Add(ckbxOption2);
+            grpaddOn.Controls.Add(ckbxOption1);
+            grpaddOn.Location = new Point(360, 44);
+            grpaddOn.Margin = new Padding(3, 2, 3, 2);
+            grpaddOn.Name = "grpaddOn";
+            grpaddOn.Padding = new Padding(3, 2, 3, 2);
+            grpaddOn.Size = new Size(220, 114);
+            grpaddOn.TabIndex = 1;
+            grpaddOn.TabStop = false;
+            grpaddOn.Text = "Add-On Items";
             // 
             // ckbxOption9
             // 
             ckbxOption9.AutoSize = true;
-            ckbxOption9.Location = new Point(12, 106);
+            ckbxOption9.Location = new Point(10, 80);
+            ckbxOption9.Margin = new Padding(3, 2, 3, 2);
             ckbxOption9.Name = "ckbxOption9";
-            ckbxOption9.Size = new Size(111, 24);
+            ckbxOption9.Size = new Size(89, 19);
             ckbxOption9.TabIndex = 8;
             ckbxOption9.Text = "Bread Sticks";
             ckbxOption9.UseVisualStyleBackColor = true;
@@ -136,9 +141,10 @@
             // ckbxOption8
             // 
             ckbxOption8.AutoSize = true;
-            ckbxOption8.Location = new Point(11, 67);
+            ckbxOption8.Location = new Point(10, 50);
+            ckbxOption8.Margin = new Padding(3, 2, 3, 2);
             ckbxOption8.Name = "ckbxOption8";
-            ckbxOption8.Size = new Size(100, 24);
+            ckbxOption8.Size = new Size(81, 19);
             ckbxOption8.TabIndex = 7;
             ckbxOption8.Text = "Bacon Bits";
             ckbxOption8.UseVisualStyleBackColor = true;
@@ -147,9 +153,10 @@
             // ckbxOption7
             // 
             ckbxOption7.AutoSize = true;
-            ckbxOption7.Location = new Point(11, 29);
+            ckbxOption7.Location = new Point(10, 22);
+            ckbxOption7.Margin = new Padding(3, 2, 3, 2);
             ckbxOption7.Name = "ckbxOption7";
-            ckbxOption7.Size = new Size(90, 24);
+            ckbxOption7.Size = new Size(75, 19);
             ckbxOption7.TabIndex = 6;
             ckbxOption7.Text = "Croutons";
             ckbxOption7.UseVisualStyleBackColor = true;
@@ -158,9 +165,10 @@
             // ckbxOption6
             // 
             ckbxOption6.AutoSize = true;
-            ckbxOption6.Location = new Point(11, 106);
+            ckbxOption6.Location = new Point(10, 80);
+            ckbxOption6.Margin = new Padding(3, 2, 3, 2);
             ckbxOption6.Name = "ckbxOption6";
-            ckbxOption6.Size = new Size(71, 24);
+            ckbxOption6.Size = new Size(58, 19);
             ckbxOption6.TabIndex = 5;
             ckbxOption6.Text = "Olives";
             ckbxOption6.UseVisualStyleBackColor = true;
@@ -169,9 +177,10 @@
             // ckbxOption5
             // 
             ckbxOption5.AutoSize = true;
-            ckbxOption5.Location = new Point(11, 67);
+            ckbxOption5.Location = new Point(10, 50);
+            ckbxOption5.Margin = new Padding(3, 2, 3, 2);
             ckbxOption5.Name = "ckbxOption5";
-            ckbxOption5.Size = new Size(86, 24);
+            ckbxOption5.Size = new Size(69, 19);
             ckbxOption5.TabIndex = 4;
             ckbxOption5.Text = "Sausage";
             ckbxOption5.UseVisualStyleBackColor = true;
@@ -180,9 +189,10 @@
             // ckbxOption4
             // 
             ckbxOption4.AutoSize = true;
-            ckbxOption4.Location = new Point(12, 29);
+            ckbxOption4.Location = new Point(10, 22);
+            ckbxOption4.Margin = new Padding(3, 2, 3, 2);
             ckbxOption4.Name = "ckbxOption4";
-            ckbxOption4.Size = new Size(98, 24);
+            ckbxOption4.Size = new Size(80, 19);
             ckbxOption4.TabIndex = 3;
             ckbxOption4.Text = "Pepperoni";
             ckbxOption4.UseVisualStyleBackColor = true;
@@ -191,9 +201,10 @@
             // ckbxOption3
             // 
             ckbxOption3.AutoSize = true;
-            ckbxOption3.Location = new Point(11, 106);
+            ckbxOption3.Location = new Point(10, 80);
+            ckbxOption3.Margin = new Padding(3, 2, 3, 2);
             ckbxOption3.Name = "ckbxOption3";
-            ckbxOption3.Size = new Size(108, 24);
+            ckbxOption3.Size = new Size(89, 19);
             ckbxOption3.TabIndex = 2;
             ckbxOption3.Text = "French Fries";
             ckbxOption3.UseVisualStyleBackColor = true;
@@ -201,9 +212,10 @@
             // ckbxOption2
             // 
             ckbxOption2.AutoSize = true;
-            ckbxOption2.Location = new Point(11, 67);
+            ckbxOption2.Location = new Point(10, 50);
+            ckbxOption2.Margin = new Padding(3, 2, 3, 2);
             ckbxOption2.Name = "ckbxOption2";
-            ckbxOption2.Size = new Size(219, 24);
+            ckbxOption2.Size = new Size(179, 19);
             ckbxOption2.TabIndex = 1;
             ckbxOption2.Text = "Ketchup, Mustard, and Mayo";
             ckbxOption2.UseVisualStyleBackColor = true;
@@ -211,9 +223,10 @@
             // ckbxOption1
             // 
             ckbxOption1.AutoSize = true;
-            ckbxOption1.Location = new Point(11, 29);
+            ckbxOption1.Location = new Point(10, 22);
+            ckbxOption1.Margin = new Padding(3, 2, 3, 2);
             ckbxOption1.Name = "ckbxOption1";
-            ckbxOption1.Size = new Size(213, 24);
+            ckbxOption1.Size = new Size(173, 19);
             ckbxOption1.TabIndex = 0;
             ckbxOption1.Text = "Lettuce, Tomato, and Onion";
             ckbxOption1.TextAlign = ContentAlignment.MiddleCenter;
@@ -227,9 +240,11 @@
             groupBox3.Controls.Add(label3);
             groupBox3.Controls.Add(label2);
             groupBox3.Controls.Add(label1);
-            groupBox3.Location = new Point(51, 239);
+            groupBox3.Location = new Point(45, 179);
+            groupBox3.Margin = new Padding(3, 2, 3, 2);
             groupBox3.Name = "groupBox3";
-            groupBox3.Size = new Size(250, 144);
+            groupBox3.Padding = new Padding(3, 2, 3, 2);
+            groupBox3.Size = new Size(219, 108);
             groupBox3.TabIndex = 2;
             groupBox3.TabStop = false;
             groupBox3.Text = "Order Total";
@@ -237,63 +252,67 @@
             // textBox3
             // 
             textBox3.BorderStyle = BorderStyle.FixedSingle;
-            textBox3.Location = new Point(105, 99);
+            textBox3.Location = new Point(92, 74);
+            textBox3.Margin = new Padding(3, 2, 3, 2);
             textBox3.Name = "textBox3";
             textBox3.ReadOnly = true;
-            textBox3.Size = new Size(125, 27);
+            textBox3.Size = new Size(110, 23);
             textBox3.TabIndex = 5;
             // 
             // textBox2
             // 
             textBox2.BorderStyle = BorderStyle.FixedSingle;
-            textBox2.Location = new Point(105, 65);
+            textBox2.Location = new Point(92, 49);
+            textBox2.Margin = new Padding(3, 2, 3, 2);
             textBox2.Name = "textBox2";
             textBox2.ReadOnly = true;
-            textBox2.Size = new Size(125, 27);
+            textBox2.Size = new Size(110, 23);
             textBox2.TabIndex = 4;
             // 
             // textBox1
             // 
             textBox1.BorderStyle = BorderStyle.FixedSingle;
-            textBox1.Location = new Point(105, 32);
+            textBox1.Location = new Point(92, 24);
+            textBox1.Margin = new Padding(3, 2, 3, 2);
             textBox1.Name = "textBox1";
             textBox1.ReadOnly = true;
-            textBox1.Size = new Size(125, 27);
+            textBox1.Size = new Size(110, 23);
             textBox1.TabIndex = 3;
             textBox1.TextAlign = HorizontalAlignment.Center;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(12, 102);
+            label3.Location = new Point(10, 76);
             label3.Name = "label3";
-            label3.Size = new Size(87, 20);
+            label3.Size = new Size(68, 15);
             label3.TabIndex = 2;
             label3.Text = "Order Total:";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(13, 68);
+            label2.Location = new Point(11, 51);
             label2.Name = "label2";
-            label2.Size = new Size(86, 20);
+            label2.Size = new Size(69, 15);
             label2.TabIndex = 1;
             label2.Text = "Tax (7.75%):";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(31, 35);
+            label1.Location = new Point(27, 26);
             label1.Name = "label1";
-            label1.Size = new Size(68, 20);
+            label1.Size = new Size(54, 15);
             label1.TabIndex = 0;
             label1.Text = "Subtotal:";
             // 
             // btnPlaceOrder
             // 
-            btnPlaceOrder.Location = new Point(501, 248);
+            btnPlaceOrder.Location = new Point(438, 186);
+            btnPlaceOrder.Margin = new Padding(3, 2, 3, 2);
             btnPlaceOrder.Name = "btnPlaceOrder";
-            btnPlaceOrder.Size = new Size(94, 29);
+            btnPlaceOrder.Size = new Size(82, 22);
             btnPlaceOrder.TabIndex = 3;
             btnPlaceOrder.Text = "Place Order";
             btnPlaceOrder.UseVisualStyleBackColor = true;
@@ -301,29 +320,33 @@
             // 
             // button2
             // 
-            button2.Location = new Point(501, 354);
+            button2.Location = new Point(438, 266);
+            button2.Margin = new Padding(3, 2, 3, 2);
             button2.Name = "button2";
-            button2.Size = new Size(94, 29);
+            button2.Size = new Size(82, 22);
             button2.TabIndex = 4;
             button2.Text = "E&xit";
             button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // frmLunchOrder
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(696, 450);
+            ClientSize = new Size(609, 338);
             Controls.Add(button2);
             Controls.Add(btnPlaceOrder);
             Controls.Add(groupBox3);
-            Controls.Add(grpbxAddOnItems);
-            Controls.Add(groupBox1);
+            Controls.Add(grpaddOn);
+            Controls.Add(grpmainCourse);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "frmLunchOrder";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Lunch Order";
-            groupBox1.ResumeLayout(false);
-            groupBox1.PerformLayout();
-            grpbxAddOnItems.ResumeLayout(false);
-            grpbxAddOnItems.PerformLayout();
+            grpmainCourse.ResumeLayout(false);
+            grpmainCourse.PerformLayout();
+            grpaddOn.ResumeLayout(false);
+            grpaddOn.PerformLayout();
             groupBox3.ResumeLayout(false);
             groupBox3.PerformLayout();
             ResumeLayout(false);
@@ -331,8 +354,8 @@
 
         #endregion
 
-        private GroupBox groupBox1;
-        private GroupBox grpbxAddOnItems;
+        private GroupBox grpmainCourse;
+        private GroupBox grpaddOn;
         private GroupBox groupBox3;
         private RadioButton rbtnSalad;
         private RadioButton rbtnPizza;
--- DLM.LunchOrder.UI/frmLunchOrder.cs
+++ DLM.LunchOrder.UI/frmLunchOrder.cs
@@ -1,53 +1,105 @@
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
 namespace DLM.LunchOrder.UI
 {
     public partial class frmLunchOrder : Form
     {
+        
+        private const decimal AddOnPrice = 0.75m;
+
+        private decimal subtotal = 0.0m;
+        private decimal tax = 0.0m;
+        private decimal total = 0.0m;
         public frmLunchOrder()
         {
             InitializeComponent();
-            //set initial values for check boxes
-            ckbxOption1.Tag = ckbxOption2.Tag = ckbxOption3.Tag = 0.75;
-            ckbxOption4.Tag = ckbxOption5.Tag = ckbxOption6.Tag = 0.50;
-            ckbxOption7.Tag = ckbxOption8.Tag = ckbxOption9.Tag = 0.25;
+
+        }
+        private void CalculateOrder()
+        {
+            subtotal = 0.0m;
+            tax = 0.0m;
+            total = 0.0m;
+
+            // Check which main course is selected
+            if (rbtnHamburger.Checked)
+                subtotal += 6.95m;
+            else if (rbtnPizza.Checked)
+                subtotal += 5.95m;
+            else if (rbtnSalad.Checked)
+                subtotal += 4.95m;
+
+            // Check add-ons and update subtotal
+            if (ckbxOption1.Checked)
+                subtotal += AddOnPrice;
+            if (ckbxOption2.Checked)
+                subtotal += AddOnPrice;
+            if (ckbxOption3.Checked)
+                subtotal += AddOnPrice;
+            if (ckbxOption4.Checked)
+                subtotal += AddOnPrice;
+            if (ckbxOption5.Checked)
+                subtotal += AddOnPrice;
+            if (ckbxOption6.Checked)
+                subtotal += AddOnPrice;
+            if (ckbxOption7.Checked)
+                subtotal += AddOnPrice;
+            if (ckbxOption8.Checked)
+                subtotal += AddOnPrice;
+            if (ckbxOption9.Checked)
+                subtotal += AddOnPrice;
+
+            tax = subtotal * 0.0775m;
+            total = subtotal + tax;
+
+            textBox1.Text = $"Subtotal: {subtotal:C}";
+            textBox2.Text = $"Tax: {tax:C}";
+            textBox3.Text = $"Total Due: {total:C}";
+        }
+
+        private void ClearAddOnCheckmarks()
+        {
+            // Clear add-on checkmarks within the addOnsGroupBox
+            ckbxOption1.Checked = false;
+            ckbxOption2.Checked = false;
+            ckbxOption3.Checked = false;
+            ckbxOption4.Checked = false;
+            ckbxOption5.Checked = false;
+            ckbxOption6.Checked = false;
+            ckbxOption7.Checked = false;
+            ckbxOption8.Checked = false;
+            ckbxOption9.Checked = false;
 
         }
 
-        private void rbtnHamburger_CheckedChanged(object sender, EventArgs e)
+        private void ClearOrderTotals()
         {
-
-            // make checkboxes visible when the corresponding radio button is checked
-            ckbxOption1.Visible = ckbxOption2.Visible = ckbxOption3.Visible = rbtnHamburger.Checked;
-
+            textBox1.Text = "Subtotal: ";
+            textBox2.Text = "Tax: ";
+            textBox3.Text = "Total Due: ";
         }
 
-        private void rbtnPizza_CheckedChanged(object sender, EventArgs e)
+        private void grpmainCourse_CheckedChanged(object sender, EventArgs e)
         {
-            // make checkboxes visible when the corresponding radio button is checked
-            ckbxOption4.Visible = ckbxOption5.Visible = ckbxOption6.Visible = rbtnPizza.Checked;
-
+            ClearAddOnCheckmarks();
+            ClearOrderTotals();
+            CalculateOrder();
         }
 
-        private void rbtnSalad_CheckedChanged(object sender, EventArgs e)
+        private void grpaddOn_CheckedChanged(object sender, EventArgs e)
         {
-            // make checkboxes visible when the corresponding radio button is checked
-            ckbxOption7.Visible = ckbxOption8.Visible = ckbxOption9.Visible = rbtnSalad.Checked;
-            
+            ClearOrderTotals();
+            CalculateOrder();
         }
 
         private void btnPlaceOrder_Click(object sender, EventArgs e)
         {
-            double total = 0;
-            //check each checkbox and accumulate the total
-            foreach (CheckBox checkBox  in Controls.OfType<CheckBox>())
-            {
-                if (checkBox.Checked)
-                {
-                    total += Convert.ToDouble(checkBox.Tag);
-                }
-            }
-            // output total to txt box
-            textBox1.Text = Convert.ToString(total);
+            CalculateOrder();
         }
-        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
